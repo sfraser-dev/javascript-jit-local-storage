749ef826a0e3ca1bae0e48bdde00053bfde88bce
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-05-05 14:10:04 +0100

    notes

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 // Using local storage in browser (10MB max, just strings (as key-value pairs))
 // To see local storage in the browser, select: 
 // inspect - moreApps(>>) - application - local storage
+// 
+// each key must be independent (DAY : Mon, DAY: Tues, not allowed)
+// But can include JSON data in local storage to get around this "restriction"
+//
+// Default behaviour on clicking subit button is to refresh the page, e.preventDefault stops this
+// Location.reload() to clear the page (of the data that was deleted via localStorage.clear())
+//
+// Local storage is like a very primitive DB, similar idea
 
 const theKey = document.getElementById("theKey");
 const theValue = document.getElementById("theValue");
@@ -16,3 +24,5 @@ submitButton.addEventListener("click", () => {
         window.localStorage.setItem(storedKey, storedData);
     }
 });
+
+console.log(window.localStorage);
